85fda03aaf9e2c659a5579324df2a5762561a755
Mario Galindo Mejia <mgalindo9410@gmail.com>
2024-12-31 01:04:25 -0600

    Update HttpTrigger.cs

--- HttpTrigger.cs
+++ HttpTrigger.cs
@@ -18,7 +18,7 @@ namespace Company.Function
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("testing delete head branches");
+            return new OkObjectResult("OpenID Connect Testing");
         }
     }
 }
